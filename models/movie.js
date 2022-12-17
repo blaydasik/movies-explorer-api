2c596345e51d6230677e05ff5de5784e31843ba4
Алексей Куров <alexei.s.kurov@gmail.com>
2022-12-18 01:12:37 +0300

    #2 feature: schema and models for users and movies

---
+++ models/movie.js
@@ -0,0 +1,88 @@
+import mongoose from 'mongoose';
+// импортируем регулярку
+import urlRegex from '../utils/constants.js';
+
+const movieSchema = new mongoose.Schema(
+  {
+    // страна создания фильма
+    country: {
+      type: String,
+      required: [true, 'поле страна создания фильма не заполнено'],
+    },
+    // режиссёр фильма
+    director: {
+      type: String,
+      required: [true, 'поле режиссёр фильма не заполнено'],
+    },
+    // длительность фильма
+    duration: {
+      type: Number,
+      required: [true, 'поле длительность фильма не заполнено'],
+    },
+    // год выпуска фильма
+    year: {
+      type: String,
+      required: [true, 'поле год выпуска фильма не заполнено'],
+      minlength: [2, 'поле год выпуска фильма [{VALUE}] содержит менее 2 символов'],
+      maxlength: [4, 'поле год выпуска фильма [{VALUE}] содержит более 4 символов'],
+    },
+    // описание фильма
+    description: {
+      type: String,
+      required: [true, 'поле описание фильма не заполнено'],
+    },
+    // ссылка на постер к фильму
+    image: {
+      type: String,
+      required: [true, 'поле ссылка на постер к фильму не заполнено'],
+      validate: {
+        validator: (url) => urlRegex.test(url),
+        message: 'поле ссылка на постер к фильму `{VALUE}` не прошло валидацию.',
+      },
+    },
+    // ссылка на птрейлер фильма
+    trailerLink: {
+      type: String,
+      required: [true, 'поле ссылка на трейлер фильма не заполнено'],
+      validate: {
+        validator: (url) => urlRegex.test(url),
+        message: 'поле ссылка на трейлер фильма `{VALUE}` не прошло валидацию.',
+      },
+    },
+    // миниатюрное изображение постера к фильму
+    thumbnail: {
+      type: String,
+      required: [true, 'поле миниатюрное изображение постера к фильму не заполнено'],
+      validate: {
+        validator: (url) => urlRegex.test(url),
+        message: 'поле миниатюрное изображение постера к фильму `{VALUE}` не прошло валидацию.',
+      },
+    },
+    // _id пользователя, который сохранил фильм
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'user',
+      required: true,
+    },
+    // id фильма, который содержится в ответе сервиса MoviesExplorer
+    movieId: {
+      type: Number,
+      required: true,
+    },
+    // название фильма на русском языке
+    nameRU: {
+      type: String,
+      required: [true, 'поле название фильма на русском языке не заполнено'],
+    },
+    // название фильма на английском языке
+    nameEN: {
+      type: String,
+      required: [true, 'поле название фильма на английском языке не заполнено'],
+    },
+  },
+  // уберем лишний для нас ключ с версией документа
+  { versionKey: false },
+);
+
+// создадим и экспортируем модель movie
+export default mongoose.model('movie', movieSchema);
---
+++ models/user.js
@@ -0,0 +1,61 @@
+import mongoose from 'mongoose';
+import validator from 'validator';
+// импортируем модуль для хэширования
+import bcrypt from 'bcryptjs';
+// импортируем классы ошибок
+import UnathorizedError from '../errors/UnathorizedError.js';
+
+const userSchema = new mongoose.Schema(
+  {
+    // имя пользователя
+    name: {
+      type: String,
+      minlength: [2, 'поле имя `{VALUE}` содержит менее 2 символов.'],
+      maxlength: [30, 'поле имя `{VALUE}` содержит более 30 символов.'],
+      required: [true, 'поле имя не заполнено.'],
+    },
+    // почта пользователя, используемая для регистрации
+    email: {
+      type: String,
+      unique: true,
+      validate: {
+        validator: validator.isEmail,
+        message: 'поле email `{VALUE}` не прошло валидацию.',
+      },
+      required: [true, 'поле email не заполнено.'],
+    },
+    // хэш пароля
+    password: {
+      type: String,
+      required: [true, 'поле пароль не заполнено.'],
+      select: false,
+    },
+  },
+  // уберем лишний для нас ключ с версией документа
+  { versionKey: false },
+);
+
+// метод схемы для поиска пользователя по логину и паролю
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      // проверим, найден ли пользователь
+      if (user) {
+        // вернем результат проверки корректности пароля
+        return bcrypt.compare(password, user.password)
+          .then((checked) => {
+            // если пароль корректный
+            if (checked) {
+              const userWithoutPassword = user.toObject();
+              delete userWithoutPassword.password;
+              return userWithoutPassword;
+            } // если хэши не совпали отклоняем промис с ошибкой
+            throw new UnathorizedError('Указаны некорректные почта или пароль :-(');
+          });
+      } // иначе отклоняем промис с ошибкой
+      throw new UnathorizedError('Указаны некорректные почта или пароль :-(');
+    });
+};
+
+// создадим и экспортируем модель user
+export default mongoose.model('user', userSchema);
